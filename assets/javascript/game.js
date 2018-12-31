a3208dc00118d9504ff6ff898aa53b8c4ba9ae00
Mario <mrodrigu@northseattle.edu>
2018-12-30 19:09:22 -0800

    createdPsychicGameJS

---
+++ assets/javascript/game.js
@@ -0,0 +1,110 @@
+(function(){
+    
+    //variables
+
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+               "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+let letterToGuess = '';
+
+let wins = 0;
+
+let losses = 0;
+
+let guessesLeft = 10;
+
+let yourGuesses = "";
+
+
+letterToGuess = Math.floor(Math.random() * 26) + 1;
+
+//FUNCTIONS
+//==========================================================================================
+
+function updateWins() {
+    wins += 1;
+    document.querySelector("#Wins").innerHTML = "Wins: " + wins;
+}
+
+function updateLosses() {
+    losses += 1;
+}
+
+function updateGuessesLeft() {
+    
+    guessesLeft -= 1;
+    console.log(guessesLeft);
+    document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+}
+
+function updateUserGuesses(guess) {
+    
+    yourGuesses += guess + ", ";
+    document.querySelector("#Guesses").innerHTML = "Guesses Made so far: " + yourGuesses;
+    
+}
+
+function randomLetter() {
+    
+    indexOfRandomLetter = Math.floor(Math.random() * 26); //generate the index of the letter to guess
+    
+    letterToGuess = letters[indexOfRandomLetter];
+    
+    console.log(letterToGuess);
+    
+    console.log(indexOfRandomLetter);
+    
+}
+    
+function resetStage() {
+    
+    guessesLeft = 10;
+    yourGuesses = "";
+    document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+    document.querySelector("#Guesses").innerHTML = "Guesses Made so far: ";
+    
+}
+
+//MAIN
+//=============================================================================================
+
+randomLetter(); //INITIALIZE THE FIRST LETTER TO GUESS
+resetStage();
+    
+//event listeners
+document.onkeyup = function(event) {
+
+    // Determine which key was pressed, make it lowercase, and set it to the userInput variable.
+    var userInput = event.key.toLowerCase();
+    
+    if(userInput === letterToGuess) {
+        
+        alert("You Guessed Correctly");
+        updateWins();
+        yourGueses = "";
+        randomLetter(); //reset - pick new random letter
+        
+    } else if (userInput != letterToGuess) {
+        
+        updateGuessesLeft();
+        
+        updateUserGuesses(userInput);
+        
+        if (guessesLeft < 0) {
+            
+            alert("You Lose. Try again.");
+            
+            updateLosses();
+            resetStage(); //reset stage
+            
+        }
+    }
+    
+    
+    
+    
+    console.log(userInput);
+};
+    
+    
+})();
